e866e75c7ade317a0655d2871e7dcba106fac856
Thibault Gagnaux <thibault.gagnaux@bit.admin.ch>
2023-11-15 15:41:13 +0100

    feat: run tests in continous integration pipeline
    using nix-github-actions which converts package attribute sets to github
    actions.

--- flake.nix
+++ flake.nix
@@ -1,28 +1,58 @@
 {
-    description           = "learn-go-with-tests";
-    inputs.nixpkgs.url    = "github:NixOS/nixpkgs/nixos-unstable";
-
-    outputs = { self, nixpkgs }:
+    description               = "learn-go-with-tests";
+    inputs.nixpkgs.url        = "github:NixOS/nixpkgs/nixos-unstable";
+    inputs.nix-github-actions =
     {
-        devShells = nixpkgs.lib.genAttrs 
+        url                    = "github:nix-community/nix-github-actions";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    outputs = { self, nixpkgs, nix-github-actions, ... }:
+    let 
+        supportedSystems = 
         [ 
             "x86_64-darwin" 
             "aarch64-darwin"
             "x86_64-linux" 
-        ] 
+        ];
+        forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+        devShells = forAllSystems
         (system:
-            let
-                pkgs = nixpkgs.legacyPackages.${system};
-            in
+        let
+            pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+            default = pkgs.mkShell 
             {
-                default = pkgs.mkShell 
-                {
-                    buildInputs = with pkgs; 
-                    [
-                        go
-                    ];
-                };
-            }
-        );
+                buildInputs = with pkgs; 
+                [
+                    go
+                ];
+            };
+        });
+
+        packages = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+            default = pkgs.buildGoModule
+            {
+                pname        = "hello-world";
+                version      = "1.0.0";
+                src          = ./hello-world;
+                vendorSha256 = null;
+            };
+        });
+
+        githubActions = 
+        let githubRunnerSystems = 
+            nixpkgs.lib.lists.remove "aarch64-darwin" supportedSystems;
+        in
+            nix-github-actions.lib.mkGithubMatrix
+        {
+            checks = nixpkgs.lib.getAttrs githubRunnerSystems self.packages;
+        };
     };
 }
