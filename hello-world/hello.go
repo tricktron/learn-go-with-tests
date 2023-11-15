6caae312c7c32162983a646bc96dd2aa98b5a1bc
Thibault Gagnaux <thibault.gagnaux@bit.admin.ch>
2023-11-15 16:14:42 +0100

    feat: return hello world for empty greeting

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -5,6 +5,9 @@ import "fmt"
 const englishHelloPrefix = "Hello, "
 
 func Hello(name string) string {
+    if (name == "") {
+        return englishHelloPrefix + "World"
+    }
     return englishHelloPrefix + name
 }
 
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-    got  := Hello("Michael")
-    want := "Hello, Michael"
+    t.Run("Hello should return greeting with name", func(t *testing.T) {
+        got  := Hello("Michael")
+        want := "Hello, Michael"
 
-    if got != want {
-        t.Errorf("got %q want %q", got, want)
-    }
+        if got != want {
+            t.Errorf("got %q want %q", got, want)
+        }
+    })
+
+    t.Run("Hello without name should return 'Hello World'", func(t *testing.T) {
+        got   := Hello("")
+        want  := "Hello, World"
+        
+        if got != want {
+            t.Errorf("got %q want %q", got, want)
+        }
+    })
 }
