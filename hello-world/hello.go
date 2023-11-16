4a9c02ef8fe1ae4535d516c3a9436fc2cce3a2d2
Thibault Gagnaux <thibault.gagnaux@bit.admin.ch>
2023-11-16 08:32:52 +0100

    fix: use golangci-lint to fix all linter errors

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,37 +2,39 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix  = "Bonjour, "
-const spanish            = "Spanish"
-const french             = "French"
+const (
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+	spanish            = "Spanish"
+	french             = "French"
+)
 
 func Hello(name, language string) string {
+	if name == "" {
+		return englishHelloPrefix + "World"
+	}
 
-    if name == "" {
-        return englishHelloPrefix + "World"
-    }
+	prefix := greetingPrefix(language)
 
-    prefix := greetingPrefix(language)
-
-    return prefix + name
+	return prefix + name
 }
 
+//nolint:nonamedreturns
 func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
 	case spanish:
 		prefix = spanishHelloPrefix
-    default:
-        prefix = englishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
+
 	return
 }
 
+//nolint:forbidigo
 func main() {
-    fmt.Println(Hello("World", "English"))
+	fmt.Println(Hello("World", "English"))
 }
-
-
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,34 +3,40 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-    t.Run("Hello should return greeting with name", func(t *testing.T) {
-        got  := Hello("Michael", "English")
-        want := "Hello, Michael"
-        assertHelloMessage(t, got, want)
-    })
+	t.Parallel()
+	t.Run("Hello should return greeting with name", func(t *testing.T) {
+		t.Parallel()
+		got := Hello("Michael", "English")
+		want := "Hello, Michael"
+		assertHelloMessage(t, got, want)
+	})
 
-    t.Run("Hello without name should return 'Hello World'", func(t *testing.T) {
-        got   := Hello("", "English")
-        want  := "Hello, World"
-        assertHelloMessage(t, got, want)
-    })
+	t.Run("Hello without name should return 'Hello World'", func(t *testing.T) {
+		t.Parallel()
+		got := Hello("", "English")
+		want := "Hello, World"
+		assertHelloMessage(t, got, want)
+	})
 
-    t.Run("Hello in Spanish should return 'Hola' greeting", func(t *testing.T) {
-        got   := Hello("Pamela", "Spanish")
-        want  := "Hola, Pamela"
-        assertHelloMessage(t, got, want)
-    })
+	t.Run("Hello in Spanish should return 'Hola' greeting", func(t *testing.T) {
+		t.Parallel()
+		got := Hello("Pamela", "Spanish")
+		want := "Hola, Pamela"
+		assertHelloMessage(t, got, want)
+	})
 
-    t.Run("Hello in French should return 'Bonjour' greeting", func(t *testing.T) {
-        got   := Hello("Marguerite", "French")
-        want  := "Bonjour, Marguerite"
-        assertHelloMessage(t, got, want)
-    })
+	t.Run("Hello in French should return 'Bonjour' greeting", func(t *testing.T) {
+		t.Parallel()
+		got := Hello("Marguerite", "French")
+		want := "Bonjour, Marguerite"
+		assertHelloMessage(t, got, want)
+	})
 }
-        
-func assertHelloMessage(t testing.TB, got, want string) {
-    t.Helper()
-    if got != want {
-        t.Errorf("got %q want %q", got, want)
-    }
+
+func assertHelloMessage(tb testing.TB, got, want string) {
+	tb.Helper()
+
+	if got != want {
+		tb.Errorf("got %q want %q", got, want)
+	}
 }
