dfa948c033ba07dca876663246091b27eda5c42b
Thibault Gagnaux <thibault.gagnaux@bit.admin.ch>
2023-11-17 13:38:32 +0100

    feat: calculate sum of dynamically sized arrays
    with just one function by using slices.

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -9,12 +9,29 @@ import (
 func TestSum(t *testing.T) {
 	t.Parallel()
 
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("Should calculate sum of an array with size 5", func(t *testing.T) {
+		t.Parallel()
 
-	got := array.Sum(numbers)
-	want := 15
+		numbers := []int{1, 2, 3, 4, 5}
 
-	if got != want {
-		t.Errorf("got %d want %d given %v", got, want, numbers)
-	}
+		got := array.Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got %d want %d given %v", got, want, numbers)
+		}
+	})
+
+	t.Run("Should calculate sum of an array with size 4", func(t *testing.T) {
+		t.Parallel()
+
+		numbers := []int{1, 2, 3, 4}
+
+		got := array.Sum(numbers)
+		want := 10
+
+		if got != want {
+			t.Errorf("got %d want %d given %v", got, want, numbers)
+		}
+	})
 }
