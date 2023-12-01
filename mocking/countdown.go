4e83a0645776b083c9ad3a52eb40e0593ef1fe03
Thibault Gagnaux <thibault.gagnaux@bit.admin.ch>
2023-12-01 11:26:22 +0100

    feat: assert oder of sleep and write statements

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -12,14 +12,25 @@ const (
 	countdownStart = 3
 )
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
+
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	Countdown(os.Stdout, &DefaultSleeper{})
 }
--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -2,23 +2,82 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 func TestCountdown(t *testing.T) {
 	t.Parallel()
 
-	buffer := &bytes.Buffer{}
+	t.Run("Countdown prints 3 2 1 Go!", func(t *testing.T) {
+		t.Parallel()
 
-	Countdown(buffer)
+		buffer := &bytes.Buffer{}
+		spySleeper := &SpySleeper{Calls: 0}
 
-	got := buffer.String()
-	want := `3
+		Countdown(buffer, spySleeper)
+
+		got := buffer.String()
+		want := `3
 2
 1
 Go!`
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+
+		if spySleeper.Calls != 3 {
+			t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
+		}
+	})
+
+	t.Run("Sleep before every print", func(t *testing.T) {
+		t.Parallel()
+
+		spySleeperPrinter := &SpyCountdownOperations{Calls: []string{}}
+
+		Countdown(spySleeperPrinter, spySleeperPrinter)
+
+		want := []string{
+			"write",
+			"sleep",
+			"write",
+			"sleep",
+			"write",
+			"sleep",
+			"write",
+		}
+
+		if !reflect.DeepEqual(want, spySleeperPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleeperPrinter.Calls)
+		}
+	})
 }
+
+type SpySleeper struct {
+	Calls int
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(_ []byte) (int, error) {
+	s.Calls = append(s.Calls, write)
+
+	return len(s.Calls), nil
+}
+
+const (
+	sleep = "sleep"
+	write = "write"
+)
