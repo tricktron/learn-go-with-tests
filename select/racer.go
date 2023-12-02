0455137f0bf93645beaf7a260fd2dad8132b21d1
Thibault Gagnaux <thibault.gagnaux@bit.admin.ch>
2023-12-02 20:36:47 +0100

    feat: add a test to fail after 10 seconds timeout

--- select/racer.go
+++ select/racer.go
@@ -1,15 +1,23 @@
 package racer
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
-func Racer(url1, url2 string) string {
+func Racer(url1, url2 string) (string, error) {
 	select {
 	case <-ping(url1):
-		return url1
+		return url1, nil
 	case <-ping(url2):
-		return url2
+		return url2, nil
+	case <-time.After(10 * time.Second): //nolint: gomnd
+		return "", fmt.Errorf( //nolint: goerr113
+			"timed out waiting for %s and %s",
+			url1,
+			url2,
+		)
 	}
 }
 
--- select/racer_test.go
+++ select/racer_test.go
@@ -12,21 +12,37 @@ import (
 func TestRacer(t *testing.T) {
 	t.Parallel()
 
-	slowServer := makeDelayedServer(20 * time.Millisecond)
-	defer slowServer.Close()
+	t.Run("returns the faster server url", func(t *testing.T) {
+		t.Parallel()
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		defer slowServer.Close()
 
-	fastServer := makeDelayedServer(0 * time.Millisecond)
-	defer fastServer.Close()
+		fastServer := makeDelayedServer(0 * time.Millisecond)
+		defer fastServer.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
-	want := fastURL
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+		want := fastURL
 
-	got := racer.Racer(slowURL, fastURL)
+		got, _ := racer.Racer(slowURL, fastURL)
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("returns an error if a server does not respond within 10s",
+		func(t *testing.T) {
+			t.Parallel()
+			serverA := makeDelayedServer(11 * time.Second)
+			serverB := makeDelayedServer(12 * time.Second)
+
+			_, err := racer.Racer(serverA.URL, serverB.URL)
+
+			if err == nil {
+				t.Error("expected an error but did not get one")
+			}
+		})
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
