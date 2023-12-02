2c879bbf6c010d9d2cf224be3e6de0de529fd492
Thibault Gagnaux <thibault.gagnaux@bit.admin.ch>
2023-12-02 19:53:04 +0100

    fix: use httptest to mock our servers and make the
    tests reproducible.

--- select/racer.go
+++ select/racer.go
@@ -7,11 +7,15 @@ import (
 
 func Racer(url1, url2 string) string {
 	startURL1 := time.Now()
-	http.Get(url1)
+
+	http.Get(url1) //nolint: errcheck,gosec,bodyclose,noctx
+
 	url1Duration := time.Since(startURL1)
 
 	startURL2 := time.Now()
-	http.Get(url2)
+
+	http.Get(url2) //nolint: errcheck,gosec,bodyclose,noctx
+
 	url2Duration := time.Since(startURL2)
 
 	if url1Duration < url2Duration {
--- select/racer_test.go
+++ select/racer_test.go
@@ -1,7 +1,10 @@
 package racer_test
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 
 	racer "learn-go-with-tests/select"
 )
@@ -9,8 +12,19 @@ import (
 func TestRacer(t *testing.T) {
 	t.Parallel()
 
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.dev"
+	slowServer := httptest.NewServer(http.HandlerFunc(
+		func(w http.ResponseWriter, _ *http.Request) {
+			time.Sleep(20 * time.Millisecond)
+			w.WriteHeader(http.StatusOK)
+		}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(
+		func(w http.ResponseWriter, _ *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	want := fastURL
 	got := racer.Racer(slowURL, fastURL)
@@ -18,4 +32,7 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
 }
