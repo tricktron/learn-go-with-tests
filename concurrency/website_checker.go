ec92fa83ae39495cb2295f6d418536033d965d1d
Thibault Gagnaux <thibault.gagnaux@bit.admin.ch>
2023-12-02 12:08:02 +0100

    refactor: use channels to sync go routine

--- concurrency/website_checker.go
+++ concurrency/website_checker.go
@@ -1,12 +1,26 @@
 package concurrency
 
-type WebsiteChecker func(string) bool
+type (
+	WebsiteChecker func(string) bool
+	result         struct {
+		string
+		bool
+	}
+)
 
-func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+func CheckWebsites(checker WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			resultChannel <- result{u, checker(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
 	}
 
 	return results
