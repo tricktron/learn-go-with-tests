0389abb26b8365c77abde5efbfe677541830ffef
Thibault Gagnaux <thibault.gagnaux@bit.admin.ch>
2023-12-02 20:19:10 +0100

    refactor: use select statement to wait on
    different channels.

--- select/racer.go
+++ select/racer.go
@@ -2,24 +2,23 @@ package racer
 
 import (
 	"net/http"
-	"time"
 )
 
 func Racer(url1, url2 string) string {
-	url1Duration := measureResponseTime(url1)
-	url2Duration := measureResponseTime(url2)
-
-	if url1Duration < url2Duration {
+	select {
+	case <-ping(url1):
 		return url1
+	case <-ping(url2):
+		return url2
 	}
-
-	return url2
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
+func ping(url string) chan struct{} {
+	channel := make(chan struct{})
+	go func() {
+		http.Get(url) //nolint: errcheck,gosec,bodyclose,noctx
+		close(channel)
+	}()
 
-	http.Get(url) //nolint: errcheck,gosec,bodyclose,noctx
-
-	return time.Since(start)
+	return channel
 }
