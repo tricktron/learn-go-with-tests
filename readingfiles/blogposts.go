95a6b73ab2cd0325e6c49147cc396ab1f7a95b14
Thibault Gagnaux <thibault.gagnaux@bit.admin.ch>
2023-12-08 21:16:04 +0100

    refactor: extract new post function to separate
    opening files from parsing files.

--- readingfiles/blogposts.go
+++ readingfiles/blogposts.go
@@ -18,7 +18,7 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	var posts []Post //nolint: prealloc
 
 	for _, file := range dir {
-		post, err := getPost(fileSystem, file)
+		post, err := getPost(fileSystem, file.Name())
 		if err != nil {
 			return nil, err
 		}
@@ -29,19 +29,23 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	return posts, nil
 }
 
-func getPost(fileSystem fs.FS, file fs.DirEntry) (Post, error) {
-	postFile, err := fileSystem.Open(file.Name())
+func getPost(fileSystem fs.FS, fileName string) (Post, error) {
+	postFile, err := fileSystem.Open(fileName)
 	if err != nil {
 		return Post{}, err //nolint: wrapcheck
 	}
 	defer postFile.Close()
 
+	return newPost(postFile)
+}
+
+func newPost(postFile io.Reader) (Post, error) {
 	postData, err := io.ReadAll(postFile)
 	if err != nil {
 		return Post{}, err //nolint: wrapcheck
 	}
 
-	post := Post{Title: string(postData)[7:]}
+	post := Post{Title: string(postData[7:])}
 
 	return post, nil
 }
