acb1168aaeafb1c6b07bc50c2ed661711212974a
Thibault Gagnaux <thibault.gagnaux@bit.admin.ch>
2023-12-11 16:57:04 +0100

    build-an-application refactor: use in-memory store
    for main code.

--- build-an-application/main.go
+++ build-an-application/main.go
@@ -7,7 +7,13 @@ import (
 	"learn-go-with-tests/build-an-application/server"
 )
 
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(_ string) int {
+	return 123 //nolint: gomnd
+}
+
 func main() {
-	server := &server.PlayerServer{Store: nil}
+	server := &server.PlayerServer{Store: &InMemoryPlayerStore{}}
 	log.Fatal(http.ListenAndServe(":5000", server)) //nolint: gosec
 }
