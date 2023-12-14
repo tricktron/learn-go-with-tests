8a79e5284416acfcff9931f9e2662b5ac39906f4
Thibault Gagnaux <thibault.gagnaux@bit.admin.ch>
2023-12-14 14:13:11 +0100

    build-an-application feat: return league table as
    json.

--- build-an-application/server.go
+++ build-an-application/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strings"
@@ -14,6 +15,11 @@ type PlayerStore interface {
 type PlayerServer struct {
 	Store PlayerStore
 	http.Handler
+}
+
+type Player struct {
+	Name string
+	Wins int
 }
 
 func NewPlayerServer(store PlayerStore) *PlayerServer {
@@ -31,6 +37,11 @@ func NewPlayerServer(store PlayerStore) *PlayerServer {
 }
 
 func (p *PlayerServer) leagueHandler(writer http.ResponseWriter, _ *http.Request) {
+	leagueTable := []Player{
+		{"Chris", 20},
+	}
+
+	json.NewEncoder(writer).Encode(leagueTable) //nolint: errcheck,errchkjson
 	writer.WriteHeader(http.StatusOK)
 }
 
--- build-an-application/server_test.go
+++ build-an-application/server_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -101,6 +102,17 @@ func TestLeague(t *testing.T) {
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
+
+		var got []Player
+
+		err := json.NewDecoder(response.Body).Decode(&got)
+		if err != nil {
+			t.Fatalf(
+				"Unable to parse response from server %q into slice of Player '%v'",
+				response.Body,
+				err,
+			)
+		}
 
 		assertStatusCode(t, response.Code, http.StatusOK)
 	})
